b5d885a992f897e4e89c8345804dc969a8a71d54
Sruthi Raghavan <68578094+Sruthi-2002@users.noreply.github.com>
2022-04-27 09:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
+import pyjokes
 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
+lss = sr.Recognizer()
+eg = pyttsx3.init()
+voices = eg.getProperty('voices')
 
-engine.setProperty('voice', voices[1].id)
+eg.setProperty('voice', voices[1].id)
 
 def talk(text):
-    engine.say(text)
-    engine.runAndWait()
+    eg.say(text)
+    eg.runAndWait()
 
-engine.say('Hi I am your personal assistant, Cubic')
-engine.say('What can I do for you?')
-engine.runAndWait()
+eg.say('Hi I am your personal assistant, Cubic')
+eg.say('What can I do for you?')
+eg.runAndWait()
 
 
 def take_command():
   try:
     with sr.Microphone() as source:
         print('listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
+        voice = lss.listen(source)
+        command = lss.recognize_google(voice)
         command = command.lower()
         if 'cubic' in command:
             command = command.replace('cubic', '')
@@ -32,7 +33,7 @@ def take_command():
   except:
     pass
   return command
-def run_alexa():
+def cubic():
     command = take_command()
     print(command)
     if 'play' in command:
@@ -48,6 +49,17 @@ def run_alexa():
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+        print('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+        print('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+        print(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
 
 
-run_alexa()
+cubic()
